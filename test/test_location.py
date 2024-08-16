1b1a52b4b2f469b50048a2c471062a469a7f51c0
ShibelAl <shibelalshech300@gmail.com>
2024-08-16 09:29:01 +0300

    tests file, contains tests about job location

---
+++ test/test_location.py
@@ -0,0 +1,31 @@
+import unittest
+from infra.api_wrapper import APIWrapper
+from infra.config_provider import ConfigProvider
+from logic.location import Location
+
+
+class TestCompany(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Set up the test environment by loading the configuration and initializing the API wrapper,
+        and then making a JobAPIs object, its API response and its json body.
+        """
+        self._config = ConfigProvider.load_config_json()
+        self._api_request = APIWrapper()
+        self.location = Location(self._api_request)
+        self.response = self.location.search_locations()
+        self.response_body = self.response.json()
+
+    def test_if_locations_is_in_berlin(self):
+        """
+        Tests if all location names in the response contain the substring 'Berlin'.
+
+        - Asserts that the response status code is 200.
+        - Asserts that all location names contain 'Berlin'.
+        """
+        # Act
+        all_names_contain_berlin = Location(self._api_request).all_names_contain_berlin(self.response_body)
+        # Assert
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTrue(all_names_contain_berlin)
